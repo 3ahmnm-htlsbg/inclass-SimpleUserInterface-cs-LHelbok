4986e2e0a17329176de9f34e55282686258499e6
LH03 <lea.helbok03@htl-salzburg.ac.at>
2020-01-07 14:49:00 +0100

    add script, made a working counter where when you click a button a number adds one up

---
+++ Assets/MyGame/Scripts/UserInterface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+    public Text countOne;
+    public int counter;
+    
+    void Start()
+    {
+        counter = 0;
+    }
+
+    private int AddOne()
+    {
+        counter++;
+        return counter;   
+    }
+
+    public void ShowCounter()
+    {
+            countOne.text = AddOne().ToString();
+        
+    }
+}
